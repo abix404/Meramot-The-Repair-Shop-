69811964af196d9101a5e924364fd3054e087eeb
Abid Muhammad <abidmuhammad206@gmail.com>
2025-03-30 03:12:19 +0600

    Update 3.1

--- meramot/urls.py
+++ meramot/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import path
 from meramotapp import views as views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', views.home, name='home'),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
